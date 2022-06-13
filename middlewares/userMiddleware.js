77b9ac80731926508e635b9b44edcfeba5ea6450
PallaviJeengar <pallavijeengar@gmail.com>
2022-06-13 18:36:51 +0530

    login api

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 exports.authenticateToken=(req, res, next)=> {
+  // console.log("middleware");
   const token = req.headers['authorization']
-  console.log(authHeader);
+  console.log(token);
   if (token == null) return res.sendStatus(401)
-
+  
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
     console.log(err)
 
     if (err) return res.sendStatus(403)
 
     req.user = user
-
+    console.log(req.user);
     next()
   })
 }
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
 exports.generateAccessToken=(username)=>{
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+    return jwt.sign(username, process.env.TOKEN_SECRET);
 }
