fdce185b487512d1c95ab54026c95aa8f54600c8
PallaviJeengar <pallavijeengar@gmail.com>
2022-06-13 13:00:44 +0530

    swagger documentation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,184 @@
+// routes/users.js
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         firstName:
+ *           type: string
+ *         lastName: 
+ *           type: string
+ *         email: 
+ *           type: string
+ *         mobileNumber: 
+ *           type: string
+ *         bookIsseued: 
+ *            type: string
+ *         address: 
+ *            type: string
+ *         password: 
+ *            type: string
+ */
+
 var express = require('express');
 var router = express.Router();
 
 const userController=require('../controllers/userController');
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
 router.get('/user',userController.showUsers);
+
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Retrieve a list of JSONPlaceholder users.
+ *     description: Retrieve a list of users from JSONPlaceholder.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                          $ref: '#/components/schemas/User'
+ *                      
+ */
 router.post('/user',userController.addUser);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   patch:
+ *     summary: Retrieve a list of JSONPlaceholder users.
+ *     description: Retrieve a list of users from JSONPlaceholder.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                         $ref: '#/components/schemas/User'
+ *                      
+ */
 router.patch('/user/:id',userController.updateUser);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     summary: Retrieve a list of JSONPlaceholder users.
+ *     description: Retrieve a list of users from JSONPlaceholder.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                 items:
+ *                       $ref: '#/components/schemas/User'
+ *                      
+ */
 router.delete('/user/:id',userController.deleteUser);
+
+/**
+ * @swagger
+ * /user/issueBook/{id}/{uid}:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users.
+ *     description: Retrieve a list of users from JSONPlaceholder.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: uid
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                       firstName:            
+ *                         type: string
+ *                       lastName:            
+ *                         type: string
+ *                       email:            
+ *                         type: string
+ *                       mobileNumber:        
+ *                         type: string
+ *                       bookIsseued:            
+ *                         type: string
+ *                       address:            
+ *                         type: string
+ *                       password:            
+ *                         type: string
+ *                      
+ */
 router.get('/user/issueBook/:id/:uid',userController.issueBook);
 
 module.exports = router;
